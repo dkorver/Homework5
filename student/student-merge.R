85cd126e18d8a16d75b70f552c1f80bad86514fc
dkorver <danekorver@outlook.com>
2024-10-08 22:30:14 -0400

    Add files via upload

--- student/student-merge.R
+++ student/student-merge.R
@@ -17,3 +17,58 @@ d3_selected=inner_join(d1_selected,d2_selected,by=c("school","sex","age","addres
 
 class(d1)
 typeof(d1)
+
+sum_na <- function(column){
+  sum(is.na(column))
+}
+
+na_counts <- d3_selected |>
+  summarize(across(everything(), sum_na))
+na_counts
+
+#Medu Fedu sex famsize
+d3_selected <- d3_selected |>
+  mutate(SexF = factor(sex, levels = c("F", "M"), labels = c("Female", "Male")),
+         MeduF = as.factor(Medu),
+         FeduF = as.factor(Fedu),
+         famsizeF = as.factor(famsize))
+
+d3_selected |> count(Medu)
+d3_selected |> count(Fedu)
+d3_selected |> count(sex,SexF)
+d3_selected |> count(famrel.x)
+d3_selected |> count(traveltime.x)
+
+#age,absences,G3 G1 G2 G3
+d1 <- d1 |>
+  mutate(SexF = factor(sex, levels = c("F", "M"), labels = c("Female", "Male")),
+         MeduF = as.factor(Medu),
+         FeduF = as.factor(Fedu),
+         addressF = as.factor(address),
+         famsizeF = as.factor(famsize))
+
+d1 |>
+  select(SexF,age,absences,G3) |>
+  group_by(SexF) |>
+  drop_na(SexF) |>
+  summarize(across(where(is.numeric), 
+                   list("mean" = ~ mean(.x, na.rm = TRUE), 
+                        "median" = ~ median(.x, na.rm = TRUE),
+                        "sd" = ~sd(.x, na.rm = TRUE),
+                        "iqr" = ~IQR(.x, na.rm = TRUE)), 
+                   .names = "{.fn}_{.col}"))
+
+d1 |>
+  select(addressF,famsizeF,SexF,age,absences,G3) |>
+  group_by(addressF,famsizeF) |>
+  drop_na(SexF) |>
+  summarize(across(where(is.numeric), 
+                   list("mean" = ~ mean(.x, na.rm = TRUE), 
+                        "median" = ~ median(.x, na.rm = TRUE),
+                        "sd" = ~sd(.x, na.rm = TRUE),
+                        "iqr" = ~IQR(.x, na.rm = TRUE)), 
+                   .names = "{.fn}_{.col}"))
+
+
+
+
